387da2af779f463fe86d916c5df2e96936f8a979
Polakova <kristine.polakova@accenture.com>
2024-01-17 18:14:32 +0200

    added style to cat cards

--- js/example-api.js
+++ js/example-api.js
@@ -1,114 +1,160 @@
-const originalDiv = document.querySelector("#templateDiv");
+//example from lectures
 
-async function getElixirs() {
-  try {
-    //I will send a request and then parse data
-    const response = await fetch(
-      "https://wizard-world-api.herokuapp.com/Elixirs"
-    );
+// const originalDiv = document.querySelector("#templateDiv");
 
-    const data = await response.json(); //Still a promise
+// async function getElixirs() {
+//   try {
+//     //I will send a request and then parse data
+//     const response = await fetch(
+//       "https://wizard-world-api.herokuapp.com/Elixirs"
+//     );
 
-    for (elixir of data) {
-      if (elixir.ingredients.length != 0) {
-        for (ingredients of elixir.ingredients) {
-          let fullInfo = await fetch(
-            `https://wizard-world-api.herokuapp.com/Ingredients/${ingredients.id}`
-          );
-          ingredient = await fullInfo.json();
-        }
-      }
-    }
-    return data;
-  } catch (e) {
-    console.error(e);
-  }
-}
+//     const data = await response.json(); //Still a promise
 
-function fillCardData(card, elixir, i) {
-  card.setAttribute("id", "position-" + i);
-  card.querySelector(".card-title").innerText = elixir.name;
-  // card.querySelector(".card-subtitle").innerText = "Difficulty: " + elixir.difficulty;
-  card.querySelector(".card-text").innerText = "Effects: " + elixir.effect;
-}
+//     for (elixir of data) {
+//       if (elixir.ingredients.length != 0) {
+//         for (ingredients of elixir.ingredients) {
+//           let fullInfo = await fetch(
+//             `https://wizard-world-api.herokuapp.com/Ingredients/${ingredients.id}`
+//           );
+//           ingredient = await fullInfo.json();
+//         }
+//       }
+//     }
+//     return data;
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
 
-async function generateContent(div) {
-  let cloned;
-  let clonedCard;
-  let card;
-  let data = await getElixirs();
+// function fillCardData(card, elixir, i) {
+//   card.setAttribute("id", "position-" + i);
+//   card.querySelector(".card-title").innerText = elixir.name;
+//   // card.querySelector(".card-subtitle").innerText = "Difficulty: " + elixir.difficulty;
+//   card.querySelector(".card-text").innerText = "Effects: " + elixir.effect;
+// }
 
-  data.forEach(function (value, i) {
-    if (i % 3 === 0) {
-      //I begin new div with first card
-      cloned = originalDiv.cloneNode(true);
-      clonedCard = cloned.querySelector(".col").cloneNode(true);
-      card = clonedCard.querySelector(".card");
-      fillCardData(card, value, i);
-      cloned.appendChild(clonedCard);
-    } else if (i % 3 === 1) {
-      clonedCard = cloned.querySelector(".col").cloneNode(true);
-      card = clonedCard.querySelector(".card");
-      fillCardData(card, value, i);
-      cloned.appendChild(clonedCard);
-    } else {
-      clonedCard = cloned.querySelector(".col").cloneNode(true);
-      card = clonedCard.querySelector(".card");
-      fillCardData(card, value, i);
-      cloned.appendChild(clonedCard);
-      cloned.removeChild(cloned.querySelector(".col"));
-      document.querySelector("#api-container").appendChild(cloned);
-    }
-  });
-  document.querySelector("#api-container").removeChild(originalDiv);
-}
+// async function generateContent(div) {
+//   let cloned;
+//   let clonedCard;
+//   let card;
+//   let data = await getElixirs();
 
-generateContent(originalDiv);
+//   data.forEach(function (value, i) {
+//     if (i % 3 === 0) {
+//       //I begin new div with first card
+//       cloned = originalDiv.cloneNode(true);
+//       clonedCard = cloned.querySelector(".col").cloneNode(true);
+//       card = clonedCard.querySelector(".card");
+//       fillCardData(card, value, i);
+//       cloned.appendChild(clonedCard);
+//     } else if (i % 3 === 1) {
+//       clonedCard = cloned.querySelector(".col").cloneNode(true);
+//       card = clonedCard.querySelector(".card");
+//       fillCardData(card, value, i);
+//       cloned.appendChild(clonedCard);
+//     } else {
+//       clonedCard = cloned.querySelector(".col").cloneNode(true);
+//       card = clonedCard.querySelector(".card");
+//       fillCardData(card, value, i);
+//       cloned.appendChild(clonedCard);
+//       cloned.removeChild(cloned.querySelector(".col"));
+//       document.querySelector("#api-container").appendChild(cloned);
+//     }
+//   });
+//   document.querySelector("#api-container").removeChild(originalDiv);
+// }
 
-//If you do not wish to have a function as getElixirs(), here is a function to just get all records from the database
+// generateContent(originalDiv);
 
-async function getAllObjects() {
-  try {
-    const response = await fetch(
-      "https://wizard-world-api.herokuapp.com/Elixirs"
-    );
+// //If you do not wish to have a function as getElixirs(), here is a function to just get all records from the database
 
-    const data = await response.json(); //Still a promise
-    return data;
-  } catch (e) {
-    console.error(e);
-  }
-}
+// async function getAllObjects() {
+//   try {
+//     const response = await fetch(
+//       "https://wizard-world-api.herokuapp.com/Elixirs"
+//     );
 
-//Get specific wizard object
+//     const data = await response.json(); //Still a promise
+//     return data;
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
 
-async function getSpecificObject(parameter) {
-  try {
-    let fullWizardInfo = await fetch(
-      `https://wizard-world-api.herokuapp.com/Wizards/${parameter}`
-      //"https://wizard-world-api.herokuapp.com/Wizards/" + inventor.id
-    );
-    let singleObj = await fullWizardInfo.json();
-    return singleObj;
-  } catch (e) {
-    console.error(e);
-  }
-}
+// //Get specific wizard object
 
-//A more generic function
-async function getListOfObjectsApi(url) {
-  try {
-    return (await fetch(url)).json();
-  } catch (e) {
-    console.error(e);
-  }
-}
+// async function getSpecificObject(parameter) {
+//   try {
+//     let fullWizardInfo = await fetch(
+//       `https://wizard-world-api.herokuapp.com/Wizards/${parameter}`
+//       //"https://wizard-world-api.herokuapp.com/Wizards/" + inventor.id
+//     );
+//     let singleObj = await fullWizardInfo.json();
+//     return singleObj;
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
 
-//A more generic function with param
-async function getListOfObjectsApi(url, param) {
-  try {
-    return (await fetch(url)).json();
-  } catch (e) {
-    console.error(e);
-  }
-}
+// //A more generic function
+// async function getListOfObjectsApi(url) {
+//   try {
+//     return (await fetch(url)).json();
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
+
+// //A more generic function with param
+// async function getListOfObjectsApi(url, param) {
+//   try {
+//     return (await fetch(url)).json();
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
+
+// async function getCatFact() {
+//   try {
+//     // Fetching a random cat fact
+//     const response = await fetch("https://cat-fact.herokuapp.com/facts/random");
+//     const data = await response.json();
+//     return {
+//       name: "Random Cat Fact",
+//       text: data.text,
+//     };
+//   } catch (e) {
+//     console.error(e);
+//     return {
+//       name: "Error",
+//       text: "Could not load cat fact.",
+//     };
+//   }
+// }
+
+// async function generateContent() {
+//   let container = document.querySelector("#api-container");
+//   let originalDiv = document.querySelector("#templateDiv");
+//   if (!container || !originalDiv) {
+//     console.error("Required elements not found in the DOM");
+//     return;
+//   }
+
+//   let cloned = originalDiv.cloneNode(true);
+//   let card = cloned.querySelector(".card");
+//   let cardData = await getCatFact(); // Fetch the random cat fact
+
+//   // Set the card data with the cat fact
+//   card.querySelector(".card-title").innerText = cardData.name;
+//   card.querySelector(".card-text").innerText = cardData.text;
+
+//   // Append the filled card to the container
+//   container.appendChild(cloned);
+//   originalDiv.remove(); // This removes the template div from the DOM
+// }
+
+// generateContent();
+
+// // Initialize the content generation
+// generateContent(document.querySelector("#templateDiv"));
--- js/index.js
+++ js/index.js
@@ -1,61 +1,61 @@
-// function setupDOMEvents() {
-//   const introText = document.querySelector(".intro-text");
-//   if (introText) {
-//     introText.addEventListener("mouseover", function () {
-//       this.classList.add("intro-text-mouse-over");
-//     });
+function setupDOMEvents() {
+  const introText = document.querySelector(".intro-text");
+  if (introText) {
+    introText.addEventListener("mouseover", function () {
+      this.classList.add("intro-text-mouse-over");
+    });
 
-//     introText.addEventListener("mouseout", function () {
-//       this.classList.remove("intro-text-mouse-over");
-//     });
-//   }
+    introText.addEventListener("mouseout", function () {
+      this.classList.remove("intro-text-mouse-over");
+    });
+  }
 
-//   const loginBtn = document.getElementById("loginBtn");
-//   if (loginBtn) {
-//     loginBtn.addEventListener("click", function () {
-//       console.log("Log in button clicked");
-//     });
-//   }
+  const loginBtn = document.getElementById("loginBtn");
+  if (loginBtn) {
+    loginBtn.addEventListener("click", function () {
+      console.log("Log in button clicked");
+    });
+  }
 
-//   const firstInput = document.querySelector("input");
-//   if (firstInput) {
-//     firstInput.addEventListener("focus", function () {
-//       this.style.outline = "2px solid blue";
-//     });
+  const firstInput = document.querySelector("input");
+  if (firstInput) {
+    firstInput.addEventListener("focus", function () {
+      this.style.outline = "2px solid blue";
+    });
 
-//     firstInput.addEventListener("blur", function () {
-//       this.style.outline = "";
-//     });
-//   }
+    firstInput.addEventListener("blur", function () {
+      this.style.outline = "";
+    });
+  }
 
-//   window.addEventListener("scroll", function () {
-//     console.log("Scrolling...");
-//   });
+  window.addEventListener("scroll", function () {
+    console.log("Scrolling...");
+  });
 
-//   window.addEventListener("resize", function () {
-//     console.log(
-//       "Window resized to:",
-//       window.innerWidth,
-//       "x",
-//       window.innerHeight
-//     );
-//   });
+  window.addEventListener("resize", function () {
+    console.log(
+      "Window resized to:",
+      window.innerWidth,
+      "x",
+      window.innerHeight
+    );
+  });
 
-//   window.addEventListener("load", function () {
-//     console.log("Page fully loaded");
-//   });
+  window.addEventListener("load", function () {
+    console.log("Page fully loaded");
+  });
 
-//   const imageElement = document.querySelector("img");
-//   imageElement.addEventListener("error", function () {
-//     console.log("Image failed to load:", this.src);
-//   });
+  const imageElement = document.querySelector("img");
+  imageElement.addEventListener("error", function () {
+    console.log("Image failed to load:", this.src);
+  });
 
-//   document.addEventListener("mousemove", function (event) {
-//     console.log("Mouse position:", event.clientX, event.clientY);
-//   });
-// }
+  document.addEventListener("mousemove", function (event) {
+    console.log("Mouse position:", event.clientX, event.clientY);
+  });
+}
 
-// document.addEventListener("DOMContentLoaded", setupDOMEvents);
+document.addEventListener("DOMContentLoaded", setupDOMEvents);
 
 document.addEventListener("DOMContentLoaded", (event) => {
   const observer = new IntersectionObserver(
